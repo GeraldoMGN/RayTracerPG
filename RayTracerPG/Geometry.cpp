6ca9f123e27c04b8c4c05bacedcd07e21c12d3e7
GeraldoMGN <gmgn@cin.ufpe.br>
2018-11-09 01:18:08 -0200

    Interseção de esfera

--- RayTracerPG/Geometry.cpp
+++ RayTracerPG/Geometry.cpp
@@ -1,11 +1,41 @@
 #include "Geometry.h"
+#include <iostream>
 
 Sphere::Sphere(Vec3 center, double radius) : center(center), radius(radius){}
 
-bool Sphere::intersect(const Ray & r, ObjectIntersection * info) const
+bool Sphere::intersect(const Ray& r, ObjectIntersection* info) const
+	//comentei tudo para eu entender heheheheh
 {
-	//TODO
-	return false;
+	/*esse algoritmo é uma resolução geométrica para achar o parametro da equação parametrica(t)
+	que representa onde o vetor fez a interseção com a esfera*/
+	/*envolve achar o t que faz que chega mais perto do centro da esfera (tca) fazendo o produto
+	escalar de L(vetor entre a origem do raio e o centro da esfera) e o vetor de direção, retornando
+	a o tamanho do vetor L projetado no vetor de direção(tca)*/
+	Vec3 L = this->center - r.getOrigin();
+	double tca = L.dotProduct(r.getDirection());
+	if (tca < 0.0f) {									//a interseção é atrás da camera
+		return false;
+	}
+	/*sabendo o tca, temos que achar o thc que é onde o o parametro que corresponde ao meio caminho
+	do vetor dentro da esfera, para então subitrair com o tca e achar o ponto de interseção do vetor 
+	com a esfera mais próximo*/
+	//triangulo: d(distancia do centro da esfera ao ponto mais proximo do vetor), raio e thc
+	double dSquared = L.dotProduct(L) - tca * tca;		//pitagoras
+	double rSquared = this->radius * this->radius;
+	if (dSquared > rSquared)		//o vetor não passa pela esfera
+		return false;
+	double thc = sqrt(rSquared - dSquared);
+	
+	//pontos de interseção do vetor com a "casca" da esfera
+	double t0 = tca - thc;
+	double t1 = tca + thc;
+	if (t0 < 0.0f && t1 < 0.0f) //ambas interseções atrás da camera 
+		return false;
+	if (t0 > t1 && t1 > 0.0f) 
+		info->t = t1;
+	if (t0 < t1 && t0 > 0.0f) 
+		info->t = t0;
+	return true;
 }
 
 Vec3 Sphere::getPoint() const
--- RayTracerPG/Object.cpp
+++ RayTracerPG/Object.cpp
@@ -1,3 +1,8 @@
 #include "Object.h"
 
 Object::Object(Geometry* geometry, Material* materal) : geometry(geometry), material(material) {}
+
+bool Object::intersect(const Ray& r, ObjectIntersection* info)
+{
+	return geometry->intersect(r, info);
+}
