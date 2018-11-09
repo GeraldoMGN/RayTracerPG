590110e373bbb3fdc147da489f18f24d2e1f3ca7
GeraldoMGN <gmgn@cin.ufpe.br>
2018-11-09 01:20:20 -0200

    Main progress dump

--- RayTracerPG/Main.cpp
+++ RayTracerPG/Main.cpp
@@ -2,6 +2,7 @@
 #include "Image.h"
 #include "Scene.h"
 #include "Geometry.h"
+#include "ObjectIntersection.h"
 
 #include <float.h>
 #include <iostream>
@@ -16,18 +17,23 @@ int imageHeight = 400;
 void render(Image& image, Scene& scene, Camera& camera) {
 	for (int y = 0; y < image.getHeight(); y++) {
 		for (int x = 0; x < image.getWidth(); x++) {
-			//camera fixa em 0,0,0
+			//camera fixa em 0,0,0 (necessita de uma matriz cameraToWorld)
 			double aspectRatio = (double)imageWidth / (double)imageHeight;
-			double Px = (2 * ((x + 0.5) / imageWidth) - 1) * tan(camera.getFov() / 2 * PI / 180) * aspectRatio;
-			double Py = (1 - 2 * ((y + 0.5) / imageHeight) * tan(camera.getFov() / 2 * PI / 180));
+			double Px = (2 * ((x + 0.5) / (double)imageWidth) - 1) * tan(camera.getFov() / 2 * PI / 180) * aspectRatio;
+			double Py = (1 - 2 * ((y + 0.5) / (double)imageHeight) * tan(camera.getFov() / 2 * PI / 180));
 			Vec3 origin(0.0f);
-			Vec3* direction = new Vec3(Px, Py, -1);
-			direction->normalize();
+			Vec3 direction(Px, Py, -1);
+			direction.normalize();
+			Ray ray(origin, direction);
 
 			double nearestIntersec = DBL_MAX;
 			for (int k = 0; k < scene.getNumberObjects(); k++) {
 				double intersec = DBL_MAX;
 				//if it intersects and is the intersection is less than nearestIntersec
+				ObjectIntersection* intersecInfo = new ObjectIntersection{ 0.0f, Vec3(0.0f), Vec3(0.0f), NULL };
+				if (scene.getObject(k)->intersect(ray, intersecInfo))
+					image.SetPixel(x, y, new Vec3(1.0f));
+				else image.SetPixel(x, y, new Vec3(0.0f));
 				nearestIntersec = intersec;
 			}
 		}
@@ -39,7 +45,7 @@ int main() {
 	//inicializando a imagem
 	Image image = Image(imageWidth, imageHeight);
 	//inicializando objetos e cenas
-	Sphere sphere1Geometry = Sphere(Vec3(0.0f, 0.0f, 100.0f), 10.0f);
+	Sphere sphere1Geometry = Sphere(Vec3(10.0f, 10.0f, -50.0f), 10.0f);
 	Material sphere1Material = Material(1.0f, 1.0f, 1.0f, 1.0f, Vec3(1.0f));
 	Object sphere1 = Object(&sphere1Geometry, &sphere1Material);
 	Scene scene;
