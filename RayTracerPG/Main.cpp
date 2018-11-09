206901fc7562344739911ee2a3d94e5d83c27655
GeraldoMGN <gmgn@cin.ufpe.br>
2018-11-09 03:38:57 -0200

    Agora mostra mais de um objeto na cena (medida temporária)

--- RayTracerPG/Main.cpp
+++ RayTracerPG/Main.cpp
@@ -31,11 +31,15 @@ void render(Image& image, Scene& scene, Camera& camera) {
 				double intersec = DBL_MAX;
 				//if it intersects and is the intersection is less than nearestIntersec
 				ObjectIntersection* intersecInfo = new ObjectIntersection{ 0.0f, Vec3(0.0f), Vec3(0.0f), NULL };
-				if (scene.getObject(k)->intersect(ray, intersecInfo))
-					image.SetPixel(x, y, new Vec3(1.0f));
-				else image.SetPixel(x, y, new Vec3(0.0f));
-				nearestIntersec = intersec;
+				if (scene.getObject(k)->intersect(ray, intersecInfo)) {
+					if (intersecInfo->t < nearestIntersec) {
+						image.SetPixel(x, y, new Vec3(1.0f));
+						nearestIntersec = intersecInfo->t;
+					}
+				}
 			}
+			if (nearestIntersec == DBL_MAX)
+				image.SetPixel(x, y, new Vec3(0.0f));
 		}
 	}
 	image.SaveAsPPM();
@@ -48,8 +52,11 @@ int main() {
 	Sphere sphere1Geometry = Sphere(Vec3(10.0f, 10.0f, -50.0f), 10.0f);
 	Material sphere1Material = Material(1.0f, 1.0f, 1.0f, 1.0f, Vec3(1.0f));
 	Object sphere1 = Object(&sphere1Geometry, &sphere1Material);
+	Sphere sphere2Geometry = Sphere(Vec3(-10.0f, -10.0f, -50.0f), 10.0f);
+	Object sphere2 = Object(&sphere2Geometry, &sphere1Material);
 	Scene scene;
 	scene.add(&sphere1);
+	scene.add(&sphere2);
 	Camera camera = Camera(Vec3(0.0f), Vec3(0.0f, 0.0f, 1.0f),
 		Vec3(0.0f, 1.0f, 0.0f), 90, 1.0f);
 	render(image, scene, camera);
