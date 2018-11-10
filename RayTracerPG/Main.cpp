1c2d1144d4074f184bd4a2bc8567c18cc94ebe51
GeraldoMGN <gmgn@cin.ufpe.br>
2018-11-10 18:54:20 -0200

    Mudanças para acomodar as novas lógicas da camera

--- RayTracerPG/Main.cpp
+++ RayTracerPG/Main.cpp
@@ -10,7 +10,7 @@
 //consts
 
 //ISSO DEVERIA SER LIDO DE UM ARQUIVO
-int imageWidth = 1280;
+int imageWidth = 720;
 int imageHeight = 720;
 
 void render(Image& image, Scene& scene, Camera& camera) {
@@ -41,16 +41,17 @@ int main() {
 	//inicializando a imagem
 	Image image = Image(imageWidth, imageHeight);
 	//inicializando objetos e cenas
-	Sphere sphere1Geometry = Sphere(Vec3(10.0f, 10.0f, -50.0f), 10.0f);
+	Sphere sphere1Geometry = Sphere(Vec3(10.0f, 10.0f, 50.0f), 10.0f);
 	Material sphere1Material = Material(1.0f, 1.0f, 1.0f, 1.0f, Vec3(1.0f));
 	Object sphere1 = Object(&sphere1Geometry, &sphere1Material);
-	Sphere sphere2Geometry = Sphere(Vec3(-10.0f, -10.0f, -30.0f), 10.0f);
+	Sphere sphere2Geometry = Sphere(Vec3(-10.0f, -10.0f, 50.0f), 10.0f);
 	Object sphere2 = Object(&sphere2Geometry, &sphere1Material);
 	Scene scene;
 	scene.add(&sphere1);
 	scene.add(&sphere2);
 	Camera camera = Camera(Vec3(0.0f), Vec3(0.0f, 0.0f, 1.0f),
 		Vec3(0.0f, 1.0f, 0.0f), 90, 1.0f);
+	camera.setCamToWorldMatrix();
 	render(image, scene, camera);
 
 	return 0;
