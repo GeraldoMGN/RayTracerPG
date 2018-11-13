56b1ab1de60cd82ec4b25a10528ce4cdae6525d4
GeraldoMGN <gmgn@cin.ufpe.br>
2018-11-13 19:01:05 -0200

    Funções não implementadas retiradas

--- RayTracerPG/Scene.h
+++ RayTracerPG/Scene.h
@@ -1,19 +1,12 @@
 #pragma once
 
 #include "Object.h"
-#include "ObjectIntersection.h"
-#include "Vec3.h"
 #include <vector>
 
 class Scene {
 private:
 	std::vector<Object*> objects;
 public:
-	/*Retorna true se r intersecta a cena e falso caso contrário.
-	Guarda Possíveis informações sobre a intersecção em info, caso seja provido.*/
-	bool intersect(const Ray& r, ObjectIntersection* info = nullptr) const;
-	//Implementa o modelo de iluminação escolhido, retornando uma cor.
-	Vec3 trace(const Ray& r, int recursionLevel = 0) const;
 	//Adiciona um objeto a cena.
 	void add(Object* object);
 	int getNumberObjects();
--- RayTracerPG/Vec3.h
+++ RayTracerPG/Vec3.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include<math.h>
+#include <math.h>
 
 class Vec3 {
 private:
