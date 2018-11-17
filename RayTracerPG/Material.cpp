0bd49112656d6e00da6e50c17db6275f5716d07c
GeraldoMGN <gmgn@cin.ufpe.br>
2018-11-17 21:37:52 -0200

    gets

--- RayTracerPG/Material.cpp
+++ RayTracerPG/Material.cpp
@@ -3,3 +3,28 @@
 Material::Material(double Ke, double Kd, double Ks, double alpha, Vec3 c)
 	:Ke(Ke), Kd(Kd), Ks(Ks), alpha(alpha), c(c)
 {}
+
+double Material::geKe() const
+{
+	return this->Ke;
+}
+
+double Material::getKd() const
+{
+	return this->Kd;
+}
+
+double Material::getKs() const
+{
+	return this->Ks;
+}
+
+double Material::getAlpha() const
+{
+	return this->alpha;
+}
+
+Vec3 Material::getColor() const
+{
+	return this->c;
+}
--- RayTracerPG/Material.h
+++ RayTracerPG/Material.h
@@ -5,6 +5,12 @@
 class Material {
 public:
 	Material(double Ke, double Kd, double Ks, double alpha, Vec3 c);
+
+	double geKe() const;
+	double getKd() const;
+	double getKs() const;
+	double getAlpha() const;
+	Vec3 getColor() const;
 private:
 	Vec3 c; //cor
 	double Ke, Kd, Ks, alpha; //Fator Emissivo, Difuso e Especular, Coeficiente de Rugosidade
